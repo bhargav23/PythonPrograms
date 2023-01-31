a09323bfd339a107a3a5281210fa36f28a7f82c8
bhargav23 <bhargav.kiran@gmail.com>
2023-01-31 16:31:20 +0530

    Update h2.py

--- h2.py
+++ h2.py
@@ -1,10 +1,10 @@
 """
 #sudo apt install chromium-chromedriver
 #sudo apt install python-pip
-#!pip install selenium
-#!pip install BeautifulSoup
-#!pip install pandas
-#!pip install numpy
+#!pip3 install selenium
+#!pip3 install BeautifulSoup
+#!pip3 install pandas
+#!pip3 install numpy
 """
 from selenium import webdriver
 from bs4 import BeautifulSoup
@@ -19,7 +19,7 @@ def printName(name):
         if i>0:
             name.append(str(a.text).strip())
         i = i + 1
-    print(name)
+    #print(name)
 
 #For inserting the LeaderBoard Scores in the List    
 def printScore(score):
@@ -28,18 +28,23 @@ def printScore(score):
         if(i>=2 and i%2==0):
             score.append(str(a.text).strip())
         i = i + 1
-    print(score)
+    #print(score)
 
-
-
-
-driver1 = webdriver.Chrome()
 name1 = []
 score1 = []
-driver1.get('https://www.hackerrank.com/contests/hexprac108/leaderboard')  
-content1 =driver1.page_source
-soup = BeautifulSoup(content1,features="html.parser")
-printName(name1)
-printScore(score1)
 df1 = pd.DataFrame({'Name':name1,'Score':score1})
+
+i =0
+while(True):
+    driver1 = webdriver.Chrome()
+    
+    driver1.get('https://www.hackerrank.com/contests/hexprac108/leaderboard/'+str(i))  
+    content1 =driver1.page_source
+    soup = BeautifulSoup(content1,features="html.parser")
+    printName(name1)
+    printScore(score1)
+    i=i+1
+    if(i==10):
+        break
+
 df1.to_csv('Excel_Sheet.csv',index=False,encoding="utf-8")
